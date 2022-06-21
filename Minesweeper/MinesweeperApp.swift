72bc45aa5d9c12a9fd2e442e86a8f32a4b8e2127
Michael Skiba <michael@skiba.dev>
2022-06-21 18:00:22 +0200

    Initial Commit

---
+++ Minesweeper/MinesweeperApp.swift
@@ -0,0 +1,17 @@
+//
+//  MinesweeperApp.swift
+//  Minesweeper
+//
+//  Created by Michael Skiba on 21/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct MinesweeperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
